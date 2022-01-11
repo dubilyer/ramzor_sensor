3f8cf6641a878d27aec05dca01a92c5a8fbb3f75
eduard <edaorik@gmail.com>
2022-01-11 17:34:41 +0200

    + README.md update

--- ramzor_sensor.py
+++ ramzor_sensor.py
@@ -23,7 +23,6 @@ class Ramzor(SensorEntity):
         self.client = RamzorClient(city)
         logging.getLogger("ramz").info(city)
 
-
     async def async_update(self):
         logging.getLogger("ramz").info("sending request info")
         self.native_value = await self.hass.async_add_executor_job(self.client.get_latest_grade)
